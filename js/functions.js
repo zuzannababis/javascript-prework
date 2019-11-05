7a34e863638eb40bdbe3fe79cd03ea88017cbf8d
Zuza Babis <zuzanna.babis@gmail.com>
2019-11-05 19:50:24 +0100

    Amend code

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,9 @@
 function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+	// var div = document.createElement('div');
+	// console.log(div)
+	// div.innerHTML = msg;
+	// document.getElementById('messages').appendChild(div);
+	document.getElementById('messages').innerHTML = msg;
 }
 
 function clearMessages(){
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 let computerMove = 'kamień';
 let playerMove = 'papier';
 
-printMessage('Zagrałem' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
